f54c6c53414a586540b04a9957d69ebf0acc1636
Neto test <strokova-92@mail.ru>
2023-04-18 22:55:34 +0300

    new formatting

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,9 @@
 public class BmiService {
     public int calculate(double weight, double height) {
         int index = (int) (weight / (height * height));
+        System.out.println(" Вес " + weight + " кг ");
+        System.out.println(" Рост " + height + " м ");
         return index;
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int index = service.calculate(98, 1.87);
-
-        System.out.println(index);
+        int index = service.calculate(68, 1.73);
+        System.out.println(" Индекс массы тела " + index);
     }
 }
