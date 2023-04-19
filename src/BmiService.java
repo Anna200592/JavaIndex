1fb2b49ab71aa952d73ca812b11fcd85c0af4ab4
Neto test <strokova-92@mail.ru>
2023-04-19 14:40:53 +0300

    unit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public int calculate(double weight, double height) {
-        int index = (int) (weight / (height * height));
-        System.out.println(" Вес " + weight + " кг ");
-        System.out.println(" Рост " + height + " м ");
+    public int calculate(double weightkg, double heightM) {
+        int index = (int) (weightkg / (heightM * heightM));
+        System.out.println(" Вес " + weightkg + " кг ");
+        System.out.println(" Рост " + heightM + " м ");
         return index;
 
     }
